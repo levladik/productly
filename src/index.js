2b841854c7630fd5d80ac52832dd8afad6ee2458
Levladik <vladislavtyshenko@yandex.ru>
2023-04-22 21:22:40 +0400

    fix article render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
+import { Article } from "./js/Article";
+
+const data = [
+    {
+        id: 1,
+        title: 'Increasing Prosperity With Positive Thinking',
+        urlToImage: './src/img/strategies/1.jpg',
+        tags: ['Art', 'Design'],
+        content: 'Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?',
+        date: '01.01.2020'
+    },
+    {
+        id: 2,
+        title: 'Motivation Is The First Step To Success',
+        urlToImage: './src/img/strategies/2.jpg',
+        tags: ['Culture'],
+        content: 'Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?',
+        date: '01.01.2020'
+    },
+    {
+        id: 3,
+        title: 'Success Steps For Your Personal Or Business Life',
+        urlToImage: './src/img/strategies/3.jpg',
+        tags: ['Culture', 'Design', 'Art'],
+        content: 'Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?',
+        date: '01.01.2020'
+    }
+];
+
 window.onload = function () {
-	console.log('Page Loaded')
+	console.log('Page Loaded');
+
+	// Render Articles
+
+	if (data) {
+		renderArticlesToDom();
+	}
 	// Tags
 	addTagsClickHandler();
 }
@@ -49,4 +84,25 @@ const filterStrategyBySelectedTag = (selectedTag) => {
 			}
 		});
 	});
-};
+}
+
+const renderArticlesToDom = () => {
+	let strategiesWrapper = getStrategiesWrapper();
+	generateArticles(data).forEach(article => {
+		strategiesWrapper.append(article.generateArticle()); 
+	})
+}
+
+const getStrategiesWrapper = () => {
+	const strategiesContainer = document.querySelector('.strategy-wrapper');
+	strategiesContainer.innerHTML = '';
+	return strategiesContainer;
+} 
+
+const generateArticles = (data) => {
+	let articles = [];
+	data.forEach(article => {
+		articles.push(new Article(article));
+	})
+	return articles;
+}
